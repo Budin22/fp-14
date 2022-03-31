07474337773d398964471b390425955069ae60aa
Budin <apserbud22@gmail.com>
2022-03-31 21:47:14 +0300

    done

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@
 
 let num = +prompt('Введите число')
 let result = true;
-for (let i =2; i < num; i++) {
-    if (num % i === 0) {
-        result = false;
-        break;
+
+if (num <= 1) {
+    result = false;
+} else {
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            result = false;
+            break;
+        }
     }
 }
 console.log(result);
